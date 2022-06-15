96630fa79a2073ea213b19ebe329256b74e88425
EstelleYU <iamestelleyu@gmail.com>
2022-06-15 12:51:52 +0900

    15JUN22

---
+++ src/modulo.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/* ------------------------------
+Daily Coding 15_modulo
+Wed 15 June 2022
+from CodeStates
+
+Input 2 numbers (num1, num2) and return remainder of dividing num1 by num2
+( Not using operator divider / and operator remainder % ).
+
+------------------------------ */
+
+public class modulo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" num1 입력 : ");
+        int num1 = sc.nextInt();
+        System.out.print(" num2 입력  : ");
+        int num2 = sc.nextInt();
+
+        if (num2 == 0) System.out.println("null");
+        else if (num1 == 0) System.out.println("0");
+
+        while (num1 >= num2) {
+            num1 -= num2;
+        }
+        System.out.println(num1);
+    }
+}
