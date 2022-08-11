45a1ff4615fa52cd721c425b1332ae7fe8e7deb3
EstelleYU <iamestelleyu@gmail.com>
2022-08-11 10:36:04 +0900

    Thursday, August 11th 2022 (52_uglyNumbers, Daily Coding)

---
+++ src/uglyNumbers.java
@@ -0,0 +1,79 @@
+
+/* -------------------------------
+52_uglyNumbers
+Thursday, August 11th, 2022
+Daily Coding from CodeStates
+------------------------------------
+The ugly number is a number that is divisible only by 2, 3 and 5.
+1 is the first ugly number. and ugly number are 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, ...
+Return the nth ugly number.
+------------------------------ */
+
+import java.util.ArrayList;
+
+public class uglyNumbers {
+    public static void main(String[] args) {
+        int result = uglyNumbers_f(1);
+        System.out.println(result); // --> 1
+
+        result = uglyNumbers_f(10);
+        System.out.println(result); // --> 12
+
+        result = uglyNumbers_f(30);
+        System.out.println(result); // --> 80
+    }
+    public static int uglyNumbers_f(int n) {
+        ArrayList<Integer> uglyNumberList = new ArrayList<>();
+        uglyNumberList.add(1);
+        int idx2 = 0;
+        int idx3 = 0;
+        int idx5 = 0;
+
+        for(int i = 0; i < n; i++) {
+            // 1. 가장 작은 수인 1에 2, 3, 5를 곱한 수 중에 가장 작은 수를 구한다.
+            // 2. 2가 선택됨.
+            // 3. 2는 가장 작은 수 1에 곱해졌으므로
+            //   3.1 이제 2는 그 다음 작은 수인 2에 곱해지고
+            //   3.2 3, 5는 여전히 가장 작은 수에 곱해진다.
+            // 4. 3에서 가장 작은 수는 3. 3은 이제 다음으로 작은 수인 2에 곱혀진다.
+            // 5. 반복
+            int nextMultipleOf2 = uglyNumberList.get(idx2) * 2;
+            int nextMultipleOf3 = uglyNumberList.get(idx3) * 3;
+            int nextMultipleOf5 = uglyNumberList.get(idx5) * 5;
+            int nextUglyNumber = Math.min(Math.min(nextMultipleOf2, nextMultipleOf3), nextMultipleOf5);
+            uglyNumberList.add(nextUglyNumber);
+
+            // 같은 수를 중복해서 저장할 수 있으므로,
+            // 각각 별도의 조건문으로 작성해야 한다.
+            //  2 * 3 = 6
+            //  3 * 2 = 6
+            if (nextUglyNumber == nextMultipleOf2) idx2++;
+            if (nextUglyNumber == nextMultipleOf3) idx3++;
+            if (nextUglyNumber == nextMultipleOf5) idx5++;
+        }
+        return uglyNumberList.get(n - 1);
+    }
+}
+
+/* -------- naive solution
+	 public int uglyNumbers(int n) {
+	 	int num = 0;
+	 	int count = 0;
+	 	while (n > count) {
+	 		num++;
+	 		if(isUgly(num)) count++;
+	 	}
+	 	return num;
+   }
+   public boolean isUgly(int num) {
+	 	num = decompose(num, 2);
+	 	num = decompose(num, 3);
+	 	num = decompose(num, 5);
+	 	return num == 1;
+   }
+   public int decompose(int num, int factor) {
+     while (num % factor == 0) num = num / factor;
+     return num;
+   }
+
+ */
