2420120a0d26f6916f362da1e98d13601cffd0d0
EstelleYU <iamestelleyu@gmail.com>
2022-08-12 17:48:57 +0900

    main template

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 /* -------------------------------
 
-day, August 1st, 2022
+day, August 12th 2022
 Daily Coding from CodeStates
 ------------------------------------
 
-
 ------------------------------ */
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
 
     }
 }
